9df5cfe6d2863eeef6090dbe86cc4e95880e6b2c
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-06-18 17:23:41 +0300

    feat: added serializer for PlacedBet model

--- localgames/serializers.py
+++ localgames/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Games
+from .models import Games, PlacedBets
 
 
 class GamesSerializer(serializers.ModelSerializer):
@@ -9,3 +9,9 @@ class GamesSerializer(serializers.ModelSerializer):
         fields = ['game_id', 'home', 'away', 'home_odds', 'away_odds',
                   'draw_odds', 'game_date', 'status', 'outcome',
                   ]
+
+class PlacedBetsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlacedBets
+        fields = ['user_id', 'game_id', 'placed_bet', 'placed_amount',
+                  'possible_win', 'bet_outcome',]
