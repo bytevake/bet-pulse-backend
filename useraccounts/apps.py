31de7f163088dee7a9fefdaf6e7cf15e118c1429
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-06-18 12:46:39 +0300

    feat: Registered All signals

--- useraccounts/apps.py
+++ useraccounts/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class UseraccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'useraccounts'
+
+    def ready(self):
+        """
+        Registering all signals in this app
+        """
+        from . import signals
