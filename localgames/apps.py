839ca3104a41b4a2e8a61aa25b7cab11a735fd4a
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-06-24 12:42:54 +0300

    feat: added signals to track changes in game outcomes

--- localgames/apps.py
+++ localgames/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class LocalgamesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'localgames'
+
+    def ready(self):
+        """
+        Registering all signals in this app
+        """
+        from . import signals
---
+++ localgames/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Games, PlacedBets
+from .helpers import update_normal_bet
+
+@receiver(post_save, sender=Games)
+def update_bets(sender, instance, created, **kwargs):
+    """
+    Used To Create An Account for Non Staff Users
+    """
+    # ignores creation of instance
+    if not created:
+        if instance and instance.outcome != "Pending":
+            # get all bets associated with this game
+            placed_bets = PlacedBets.objects.filter(game_id=instance.id,
+                bet_outcome="Pending")
+            on_loans = placed_bets.filter(on_loan=True)
+            normal_bets = placed_bets.filter(on_loan=False)
+
+            for norm_bet in normal_bets:
+                update_normal_bet(norm_bet, instance.outcome)
